ae64cb2b47c173b71cd67a05bca510c411b4a0f1
Deepanjan Majumdar <dipanjan122@gmail.com>
2021-02-23 17:44:11 +0000

    Anagram code

---
+++ StringsAndArrays/Anagram.go
@@ -0,0 +1,26 @@
+package main
+
+import "sort"
+
+func isAnagram (s1 string,s2 string) bool{
+  if len(s1) != len(s2){
+    return false
+  }
+  sortedS1 := SortStringByCharacter(s1)
+  sortedS2 := SortStringByCharacter(s2)
+
+  if sortedS1 == sortedS2{
+    return true
+  }else{
+    return false
+  }
+
+}
+
+func SortStringByCharacter(s string) string {
+      r := []rune(s)
+      sort.Slice(r, func(i, j int) bool {
+              return r[i] < r[j]
+      })
+      return string(r)
+}
--- StringsAndArrays/main.go
+++ StringsAndArrays/main.go
@@ -1,12 +1,14 @@
 package main
 import (
   "fmt"
+  "sort"
   )
 
 
 func main(){
    fmt.Println("Arrays And String")
    fmt.Println(HasUniqueCharsV2("iamforu"))
+   fmt.Println(isAnagramV2("dog","god123"))
 }
 
 //It is assumed that the string contains only small letters between a to z (26 Letters)
@@ -23,4 +25,29 @@ func HasUniqueCharsV2(s string)(bool){
    }
   
   return true
+}
+
+
+
+func isAnagramV2 (s1 string,s2 string) bool{
+  if len(s1) != len(s2){
+    return false
+  }
+  sortedS1 := SortStringByCharacterV2(s1)
+  sortedS2 := SortStringByCharacterV2(s2)
+
+  if sortedS1 == sortedS2{
+    return true
+  }else{
+    return false
+  }
+
+}
+
+func SortStringByCharacterV2(s string) string {
+      r := []rune(s)
+      sort.Slice(r, func(i, j int) bool {
+              return r[i] < r[j]
+      })
+      return string(r)
 }
